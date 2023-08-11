7b93ebc397bf4ac32b22308dfa72aeeef8e6f92e
za0888 <56291618+za0888@users.noreply.github.com>
2023-08-11 18:13:19 +0300

    vendor sku

--- app/enums/UserStatus.php
+++ app/enums/UserStatus.php
@@ -2,12 +2,17 @@
 
 namespace App\enums;
 
-enum UserStatus:string
+enum UserStatus: string
 
 {
-    case Administrator='administrator';
-    case Manager='manager';
-    case Customer='customer';
+    case Administrator = 'administrator';
+    case Manager = 'manager';
+    case Customer = 'customer';
+    case Accountant = "accountant";
+    case Boss = "boss";
+    case Stockkeeper = "stockkeeper";
+    case Secretary ='secretary';
+
 
     // extra helper to allow for greater customization of displayed values, without disclosing the name/value data directly
     public function label(): string
@@ -15,7 +20,11 @@ enum UserStatus:string
         return match ($this) {
             static::Administrator => 'Administrator',
             static::Manager => 'Manager',
+            static ::Accountant => "Accountant",
             static::Customer => 'Customer',
+            static:: Boss => "Boss",
+            static::Stockkeeper => "Stockkeeper",
+            static ::Secretary => "Secretary"
         };
     }
 }
