0ec5d9459472856201793e34f36e0e447553a266
za0888 <56291618+za0888@users.noreply.github.com>
2023-08-12 20:41:07 +0300

    spatie is here

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,12 +13,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use SoftDeletes;
-
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
